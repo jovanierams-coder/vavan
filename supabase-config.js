105d2e3f8f8c53a65dbcb3a1de77ff67f887bf42
jovanierams-coder <jovanierams@gmail.com>
2026-09-08 23:27:11 +0800

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://paqlcvpwbruaeigyyffi.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_xlEcBTgDLNyjK76pUuRUtg_xR9YIqSX';
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
